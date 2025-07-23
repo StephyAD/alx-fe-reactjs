e70048dff9cab67d2b223981f7ae8e986314cd7a
Stephanie <adomaa094@gmail.com>
2025-07-23 14:09:09 +0000

    added new features

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer style={{ padding: '10px', backgroundColor: '#ddd', textAlign: 'center' }}>
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+export default Footer;
