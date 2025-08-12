7abd8d4e55a00a78dfad13a4d89b422a2e4d8fae
Stephanie <adomaa094@gmail.com>
2025-08-12 02:50:39 +0000

    Added advanced interactivity and transitions

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -5,19 +5,26 @@ function UserProfile() {
                  p-4 sm:p-4 md:p-8 
                  max-w-xs sm:max-w-xs md:max-w-sm 
                  mx-auto my-20 
-                 rounded-lg shadow-lg"
+                 rounded-lg shadow-lg 
+                 hover:shadow-xl 
+                 transition-shadow duration-300 ease-in-out"
     >
       <img
         src="https://via.placeholder.com/150"
         alt="User"
         className="rounded-full 
                    w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 
-                   mx-auto"
+                   mx-auto 
+                   transform 
+                   transition-transform duration-300 ease-in-out 
+                   hover:scale-110"
       />
       <h1
         className="text-lg sm:text-lg md:text-xl 
                    text-blue-800 
-                   my-4 text-center"
+                   my-4 text-center 
+                   transition-colors duration-300 ease-in-out 
+                   hover:text-blue-500"
       >
         John Doe
       </h1>
@@ -33,4 +40,3 @@ function UserProfile() {
 }
 
 export default UserProfile;
-
